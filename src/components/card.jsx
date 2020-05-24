b56d9676f453dd1f4919b76bd44a039b7e64cd57
muhammadirzza <muhammadirzza@gmail.com>
2020-05-24 15:18:53 +0700

    third_commit

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -4,33 +4,34 @@ import '../App.css';
 const Card = () => {
     return (
         <div>
-        <div className="center"></div>
-        <ul className="cr1">
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-        <ul className="cr2">
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-        <ul className="cr3">
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-        <ul className="cr4">
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-        <h2>Selamat Hari Raya Idul Fitri 1441 H</h2>
-        <h3>Taqabbalallahu Minna Wa Minkum</h3>
+            <div className="center"></div>
+            <ul className="cr1">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <ul className="cr2">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <ul className="cr3">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <ul className="cr4">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+            <h2>Selamat Hari Raya Idul Fitri 1441 H</h2>
+            <h3>Taqabbalallahu Minna Wa Minkum</h3>
+            <p>image source : https://www.freepik.com/free-photos-vectors/mosque</p>
       </div>
     )
 }
