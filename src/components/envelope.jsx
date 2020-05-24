3b224375e514d8c7e750bfa3e348334826292357
muhammadirzza <muhammadirzza@gmail.com>
2020-05-24 10:36:54 +0700

    second_commit

--- src/components/envelope.jsx
+++ src/components/envelope.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../App.css';
 import { Link } from 'react-router-dom'
+// import {GiClick} from 'react-icons/gi'
 
 const Envelope = () => {
     
@@ -8,13 +9,19 @@ const Envelope = () => {
         <div className="wrapper">
             <div className="mail">
                 <div className="cover"></div>
-                        <Link to='/card'>
                 <div className="letter">
                     <h1>
-                            @
+                        <Link to='/card'>
+                            <div className="box">
+                                <span style={{"--i":1}}></span>
+                                <span style={{"--i":2}}></span>
+                                <span style={{"--i":3}}></span>
+                                <span style={{"--i":4}}></span>
+                                <span style={{"--i":5}}></span>
+                            </div>
+                        </Link>
                     </h1>
                 </div>
-                        </Link>
             </div>
         </div>
     )
